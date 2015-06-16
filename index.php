27afeb67628417389a89646d67d55e50e9fb8eca
Fabian Cernota <fabian.cernota@chamaeleon.de>
2015-06-16 19:48:20 +0200

    Routing und Templatehandler

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
-include("php/template-engine/template.class.php");
-include("php/template-engine/dispatcher.class.php")
-$tpl = new Template();
+$requestedTask = $_GET['task'];
+if(isset($_GET['task'])){
+ $requestedTask = $_GET['task'];
+ if(file_exists("php/handler/" . $requestedTask . ".handler.php")){
+   include("php/handler/" . $requestedTask) . ".handler.php";
 
-
-
+ }else{
+   echo "404 Datei existiert nicht";
+   echo "Requested:";
+   echo "php/handler/" . $requestedTask . ".handler.php";
+ }
+}else{
+ echo "404 Task nicht übergeben";
+}
 ?>
