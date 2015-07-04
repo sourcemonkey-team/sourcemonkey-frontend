93769b930e34e31112f03bb94beeed62dc16922f
kredo374 <moritz.kneilmann@gmx.de>
2015-07-04 19:23:30 +0200

    Möglichkeit für Mehrere Editor Tabs eingebaut

--- index.php
+++ index.php
@@ -5,8 +5,12 @@
 
   </sidebar>
   <div class="maincontent">
-      <textarea id="code" class="html">
-      </textarea>
+      <div id="tabHead">
+        <button class="tab_new tab">+</button>
+      </div>
+
+      <div id="editor_section">
+      </div>
   </div>
   <?php
   include("inc/footer.html");
--- js/editor.js
+++ js/editor.js
@@ -1,8 +1,51 @@
-function initEditor(textarea){
-    var editor = CodeMirror.fromTextArea(document.getElementById(textarea), {
-        lineNumbers: true,
-        extraKeys: {"Ctrl-Space": "autocomplete"},
-        mode: {name: "javascript", globalVars: true},
-        theme: "ambiance" 
-      });
+var editors = new Array(0);
+var activeEditor = 0;
+var $tabHead;
+var $editorSection;
+
+$(function (){
+	$tabHead = $("#tabHead");
+	$editorSection = $("#editor_section");
+
+	newTab("test.js", "js", "function test(){test++;}");
+});
+
+function newTab(fileName, language, content){
+	var id = editors.length;
+	var newEditor = new Object();
+	newEditor.name = fileName;
+	newEditor.language = language;
+	newEditor.content = content;
+
+	$tabHead.append('<div class="tab" data-editor-id="'+id+'" onClick="setActiveEditor($(this).attr(\'data-editor-id\'))"></div>');
+	$tabHead.find( "> div[data-editor-id="+id+"]" ).html(fileName + '<button class="tab_close" >x</button>')
+
+	$editorSection.append('<div data-editor-id="'+id+'" id="editor_'+id+'" class="editor"></div>');
+
+	setActiveEditor(id);
+
+	newEditor.editor = CodeMirror(document.getElementById("editor_" + id), {
+	    lineNumbers: true,
+	    extraKeys: {"Ctrl-Space": "autocomplete"},
+	    mode: {name: "javascript", globalVars: true},
+	    theme: "ambiance"
+    });
+
+	editors[id] = newEditor;
+}
+
+function setActiveEditor(id){
+	$("#tabHead div.tab[data-editor-id='"+activeEditor+"']").removeClass("active");
+	$("#editor_section div.editor[data-editor-id='"+activeEditor+"']").removeClass("active");
+	
+	activeEditor = id;
+
+	$("#tabHead div.tab[data-editor-id='"+activeEditor+"']").addClass("active");
+	$("#editor_section div.editor[data-editor-id='"+activeEditor+"']").addClass("active");
+}
+
+
+function closeTab(tab){
+	var id = $(tab).parent().attr("data-editor-id");
+	alert(id);
 }
